691adef9a7d3205f75a7da9818b9c5ae1cf2167a
Jeff Kramer <jeffkramr@gmail.com>
2014-12-10 15:48:48 -0800

    Added functions to clean up the outputs from the various functions, as well as functions to fetch professions and strains from the DB.

--- DRMax.py
+++ DRMax.py
@@ -28,6 +28,43 @@ profs = {}
 Skills = namedtuple('Skills', 'Skill, Cost')
 
 
+# Fetch Professions List
+def fetch_professions():
+    conn = sqlite3.connect('DRMax.db')
+    c = conn.cursor()
+    professions_out = list()
+
+    # Get Professions
+    c.execute('SELECT Profession FROM "Profession Skill List"')
+    professions_sql = c.fetchall()
+
+    for profession_row in professions_sql:
+        if professions_out.count(profession_row) == 0:
+            professions_out.append(profession_row)
+
+    conn.close()
+    return professions_out
+
+
+# Fetch Strain List
+def fetch_strains():
+    conn = sqlite3.connect('DRMax.db')
+    c = conn.cursor()
+    strains_out = list()
+
+    # Get Professions
+    c.execute('SELECT Strain FROM "Strain Skill List"')
+    strains_sql = c.fetchall()
+
+    for strain_row in strains_sql:
+        if strains_out.count(strain_row) == 0:
+            strains_out.append(strain_row)
+
+    conn.close()
+    return strains_out
+
+
+# Pull down skills from the DB
 def fetch_skills(strain_in, first_class_in=""):
     conn = sqlite3.connect('DRMax.db')
     c = conn.cursor()
@@ -81,7 +118,8 @@ def fetch_skills(strain_in, first_class_in=""):
 
     possible_profs_sql = c.fetchall()
 
-    for pp in possible_profs_sql:  # take all this data, and push it into a dictionary of sets of named tuples ("Skills")
+    for pp in possible_profs_sql:
+        # take all this data, and push it into a dictionary of sets of named tuples ("Skills")
         if pp[0] not in profs_out:
             profs_out[pp[0]] = set()
         test = profs_out[pp[0]]
@@ -94,46 +132,79 @@ def fetch_skills(strain_in, first_class_in=""):
     return open_skills_out, strain_skills_out, profs_out
 
 
-def maximal_skill_set(open_skills_in, strain_skills_in, profs_in):
+# Find the maximal intersecting skill set
+def maximal_skill_set(open_skills_in, strain_skills_in, profs_in, first_class_in="", second_class_in="",
+                      include_open=False, include_strain=True):
     disjoint_dict = {}
+    open_set = set()
+    strain_set = set()
+    if include_open:
+        for open_skill in open_skills_in:
+            open_set.add(open_skill.Skill)
 
+    if include_strain:
+        for strain_skill in strain_skills_in:
+            strain_set.add(strain_skill.Skill)
+
+    set1 = set()
+    set2 = set()
+    set3 = set()
     for prof1 in profs_in:
-        if firstClass != "":
-            set1 = profs_in[firstClass]
-        else:
-            set1 = profs_in[prof1]
+        set1.clear()
+        if first_class_in != "":
+            prof1 = first_class_in
+        for prof_skill in profs_in[prof1]:
+            set1.add(prof_skill.Skill)
+
         for prof2 in profs_in:
-            if secondClass != "":
-                set2 = profs_in[secondClass]
-            else:
-                set2 = profs_in[prof2]
+            set2.clear()
+            if second_class_in != "":
+                prof2 = second_class_in
+            for prof_skill in profs_in[prof2]:
+                set2.add(prof_skill.Skill)
+
             for prof3 in profs_in:
-                set3 = profs_in[prof3]
-                if firstClass != "" and secondClass == "":
-                    disjoint_dict[firstClass + ", " + prof2 + ", " + prof3] = len(set1 | set2 | set3)
-                elif firstClass != "" and secondClass != "":
-                    disjoint_dict[firstClass + ", " + secondClass + ", " + prof3] = len(set1 | set2 | set3)
-                else:
-                    disjoint_dict[prof1 + ", " + prof2 + ", " + prof3] = len(set1 | set2 | set3)
+                set3.clear()
+                for prof_skill in profs_in[prof3]:
+                    set3.add(prof_skill.Skill)
+                disjoint_dict[prof1 + ", " + prof2 + ", " + prof3] = len(set1 | set2 | set3 | open_set | strain_set)
 
     max_key = max(disjoint_dict, key=disjoint_dict.get)
     max_value = disjoint_dict[max_key]
     # print(max_key)
     print(max_value)
+    max_combos = list()
     for key, val in disjoint_dict.items():
         if val == max_value:
-            print(key)
-    max_list = max_key.split(", ")
-    max_skills = list(profs_in[max_list[0]] | profs_in[max_list[1]] | profs_in[max_list[2]])
-    max_skills.sort()
-    print(max_skills)
+            max_combos.append(key)
+    max_filtered_combos = remove_duplicate_triples(max_combos)
+    max_combos.clear()
+    for combo in max_filtered_combos:
+        max_skills = list(profs_in[combo[0]] | profs_in[combo[1]] | profs_in[combo[2]])
+        max_skills.sort()
+        max_combos.append(max_skills)
+
+    return max_filtered_combos, max_combos
 
 
-openSkills, strainSkills, profs_in = fetch_skills(strain, firstClass)
-maximal_skill_set(openSkills, strainSkills, profs)
+# Remove duplicates in a triple string (very specific for the output of maximal skill set now)
+def remove_duplicate_triples(triple_list_in):
+    filtered_list_out = list()
+    step_list = list()
+    for entry in triple_list_in:
+        inner_list = entry.split(", ")
+        inner_list.sort()
+        step_list.append(inner_list)
+
+    for entry in step_list:
+        if filtered_list_out.count(entry) == 0:
+            filtered_list_out.append(entry)
+
+    return filtered_list_out
 
 
+openSkills, strainSkills, profs = fetch_skills(strain, firstClass)
+maxComboNames, maxSkillsInCombo = maximal_skill_set(openSkills, strainSkills, profs, firstClass, secondClass)
 
-
-
-
+for name, skillset in zip(maxComboNames, maxSkillsInCombo):
+    print(name, skillset)
